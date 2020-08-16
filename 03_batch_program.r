a785e4eae6287220fe8b5cd790384ec8240d2c8a
Dee-L <david.gray.lassiter@gmail.com>
2020-08-16 09:40:48 +0200

    Fixed backup file creation

--- 03_batch_program.r
+++ 03_batch_program.r
@@ -3,13 +3,19 @@
 # Date: 2020-Jul-31
 # Version: 1.0
 
-# Revisions: Function for sourcing scripts, function for final save, inputs
-    # have 'i<dd>' prefix now
+# Revisions: Backup files being made correctly now
 # Author: David Gray Lassiter, PhD
-# Date: 2020-Aug-04
-# Revised Version: 1.1
+# Date: 2020-Aug-16
+# Revised Version: 1.2
 
 # Suggested revisions:
+# Packages that keep needing to be re-installed: reticulate, binman, testit,
+# netstat, dplyr, gtools, RSelenium, rvest. Check .libPaths() path to see if
+# they exist there. If not, try opening up an issue on Stack Overflow. Perhaps
+# it is just that VS Code doesn't install them right, but R Studio does???
+# Could add function that installs package if not installed although it doesn't
+# source it. Then it may be robust enough regardless of where the path is (in
+# case it is diff between RStudio and VS Code)
 
 # 01 Source all my functions ####
 source("04_my_fxns_this_project.r")
--- 05_scripts/s01_backup.r
+++ 05_scripts/s01_backup.r
@@ -3,12 +3,29 @@
 # Date: 2020-Jul-31
 # Version: 1.0
 
-# Revisions:
-# Author:
-# Date: YYYY-MMM-DD
-# Revised Version:
+# Revisions: Keep more backups and remove them as they age out
+# Author: David Gray Lassiter, PhD
+# Date: 2020-Aug-12
+# Revised Version: 1.1
 
+# 01 Initialize variables ####
+today <- Sys.Date()
+
+backups_to_keep <- 3
+
+make_backup_file_name <- function(day) {
+    paste0(output_folder, "01_backup_", day, output_file_name)
+}
+
+todays_file <- make_backup_file_name(today)
+
+file_to_remove <- make_backup_file_name(today - backups_to_keep)
+
+# 02 Make backup and delete aged-out backup ####
 file.copy(
     from = my_xlsx,
-    to = paste0(output_folder, "01_backup_", output_file_name), overwrite = T
+    to = todays_file,
+    overwrite = T
 )
+
+file.remove(file_to_remove)
